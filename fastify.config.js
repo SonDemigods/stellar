273cb4944a8245226aa506b76c45281c31c06ff0
demigods <a13752365641@126.com>
2025-05-08 13:13:57 +0800

    feat:将配置抽成独立文件。

---
+++ fastify.config.js
@@ -0,0 +1,11 @@
+// 参考地址: https://github.com/fastify/fastify-cli?tab=readme-ov-file#options
+module.exports = {
+  // 监听地址
+  address: "0.0.0.0",
+  // 监听端口
+  port: 3000,
+  // 日志等级
+  logLevel: "info",
+  // 是否开启日志美化
+  prettyLogs: true
+}
