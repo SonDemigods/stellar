2d85aee7bbfe54c3ebc4c4fbb3145125ec95c05e
demigods <a13752365641@126.com>
2025-07-23 15:36:50 +0800

    feat:添加数据库配置。

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 // 配置模块
 import appConfig from './config/app.config';
 import databaseConfig from './config/database.config';
+import { TypeOrmConfigService } from './config/typeOrm.config';
 
 // 应用模块
 import { CatsModule } from './cats/cats.module';
@@ -11,6 +13,10 @@ import { CatsModule } from './cats/cats.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ load: [appConfig, databaseConfig] }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeOrmConfigService,
+    }),
     CatsModule,
   ],
   controllers: [],
