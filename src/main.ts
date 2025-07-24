3f47538444d985913030a1315b0a2d0ceb505e53
demigods <a13752365641@126.com>
2025-07-24 11:32:33 +0800

    feat:添加全局响应拦截，处理响应格式。

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
-import { AppModule } from './app.module';
 
+// 全局拦截器
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
+
+// 根模块
+import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({}));
+  // 全局验证管道
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, // 自动将参数转换为 DTO 中定义的类型
+      whitelist: true, // 自动过滤掉 DTO 中没有定义的字段
+      forbidNonWhitelisted: false,
+      disableErrorMessages: false,
+    }),
+  );
+  // 全局拦截器
+  app.useGlobalInterceptors(new ResponseInterceptor());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
