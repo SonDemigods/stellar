e81813dbce82e1af7a9b9d2daee58fd1f2e5bb1b
demigods <a13752365641@126.com>
2025-09-28 14:56:50 +0800

    fix:添加基础字段的类型、名称和默认值。

--- src/common/entity/base.entity.ts
+++ src/common/entity/base.entity.ts
@@ -14,15 +14,25 @@ export abstract class BaseEntity {
   @CreateDateColumn({ type: 'datetime', name: 'create_time' })
   createTime: Date;
 
-  @Column()
+  @Column({
+    type: 'varchar',
+    name: 'create_user_id',
+    length: 36,
+    default: '0',
+  })
   createUserId: string;
 
   @UpdateDateColumn({ type: 'datetime', name: 'update_time' })
   updateTime: Date;
 
-  @Column()
+  @Column({
+    type: 'varchar',
+    length: 36,
+    name: 'update_user_id',
+    default: '0',
+  })
   updateUserId: string;
 
-  @Column({ type: 'tinyint', default: 0, name: 'delete_flag' })
+  @Column({ type: 'tinyint', name: 'delete_flag', default: 0 })
   deleteFlag: number;
 }
