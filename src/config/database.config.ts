5d84a6aa46d7452c0e5ba37114e77b1a4d11c15f
demigods <a13752365641@126.com>
2025-09-28 14:04:40 +0800

    fix:修改数据库为postgres。

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -6,5 +6,4 @@ export default registerAs('database', () => ({
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  timezone: '+08:00',
 }));
--- src/config/typeOrm.config.ts
+++ src/config/typeOrm.config.ts
@@ -7,13 +7,12 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   constructor(private readonly configService: ConfigService) {}
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
-      type: 'mysql',
+      type: 'postgres',
       host: this.configService.get<string>('database.host', 'localhost'),
-      port: this.configService.get<number>('database.port', 3306),
-      username: this.configService.get<string>('database.username', 'root'),
+      port: this.configService.get<number>('database.port', 5432),
+      username: this.configService.get<string>('database.username', 'postgres'),
       password: this.configService.get<string>('database.password', '123456'),
       database: this.configService.get<string>('database.database', 'galaxy'),
-      timezone: this.configService.get<string>('database.timezone', '+08:00'),
       autoLoadEntities: true,
     };
   }
